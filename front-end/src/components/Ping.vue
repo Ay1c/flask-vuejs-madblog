352df69444514c34479fc5afa5145028e2396ece
AmorYY <674248130@qq.com>
2020-12-30 14:27:11 +0800

    2. Vue.js通过axios访问Flask RESTful API

---
+++ front-end/src/components/Ping.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="container">
+    <button type="button" class="btn btn-primary">{{ msg }}</button>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'Ping',
+  data() {
+    return {
+      msg: ''
+    }
+  },
+  methods: {
+    getMessage () {
+      const path = 'http://localhost:5000/api/ping'
+      axios.get(path)
+        .then((response) => {
+          this.msg = response.data;
+          console.log(response);
+        })
+        .catch((error) => {
+          // eslint-disable-next-line
+          console.error(error);
+        })
+    }
+  },
+  created () {
+    this.getMessage()
+  }
+}
+</script>
